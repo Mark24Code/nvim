b9a4e387e508227391d26b413e5672cf9f4fafc4
Mark24 <mark.zhangyoung@gmail.com>
2024-07-14 01:27:29 +0800

    update: rm openai

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -101,17 +101,4 @@ return {
       "tpope/vim-fugitive",
     },
   },
-  {
-    "jackMort/ChatGPT.nvim",
-      event = "VeryLazy",
-      config = function()
-        require("chatgpt").setup()
-      end,
-      dependencies = {
-        "MunifTanjim/nui.nvim",
-        "nvim-lua/plenary.nvim",
-        "folke/trouble.nvim",
-        "nvim-telescope/telescope.nvim"
-      }
-  }
 }
