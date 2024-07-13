7447c34ad90658f9931d86bece20d092ed5c9f19
Mark24 <mark.zhangyoung@gmail.com>
2024-07-14 01:07:22 +0800

    update: Keymaps

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -101,4 +101,17 @@ return {
       "tpope/vim-fugitive",
     },
   },
+  {
+    "jackMort/ChatGPT.nvim",
+      event = "VeryLazy",
+      config = function()
+        require("chatgpt").setup()
+      end,
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim",
+        "nvim-telescope/telescope.nvim"
+      }
+  }
 }
