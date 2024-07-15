7b2a0bd8b25d37fe78b60f3269ede94753b1edb9
Mark24 <mark.zhangyoung@gmail.com>
2024-07-15 13:25:04 +0800

    update: user config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,19 +1,5 @@
 return {
   -- https://github.com/EdenEast/nightfox.nvim
   { "EdenEast/nightfox.nvim" }, -- lazy,
-
-  -- {
-  --   "nvim-web-devicons",
-  --   opts = function(_, opts)
-  --     opts.override = {
-  --       Ruby = {
-  --         icon = "",
-  --         color = "#428850",
-  --         cterm_color = "65",
-  --         name = "Zsh"
-  --       }
-  --      };
-  --   end,
-  -- }
 }
 
